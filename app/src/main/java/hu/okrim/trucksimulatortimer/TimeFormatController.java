e815d9295c63306e20461cdba5c977bd3a014c14
raczmirko <m1rk0d3v@gmail.com>
2023-10-15 19:16:57 +0200

    feat: time can now be negative on the timer to see how much delay one has

--- app/src/main/java/hu/okrim/trucksimulatortimer/TimeFormatController.java
+++ app/src/main/java/hu/okrim/trucksimulatortimer/TimeFormatController.java
@@ -2,6 +2,12 @@ package hu.okrim.trucksimulatortimer;
 
 public abstract class TimeFormatController {
     public static String createTimeText(int seconds){
+        boolean secondsAreNegative = false;
+        //If the seconds are negative we invert them
+        if(seconds < 0){
+            seconds *= -1;
+            secondsAreNegative = true;
+        }
         StringBuilder stringBuilder = new StringBuilder();
         int time = seconds;
         String h = String.valueOf(time / 3600);
@@ -9,13 +15,16 @@ public abstract class TimeFormatController {
         String m = String.valueOf(time / 60);
         time = time % 60;
         String s = String.valueOf(time);
-
-        stringBuilder.append(h.length() == 1 ? "0" + h : h);
+        if(!secondsAreNegative){
+            stringBuilder.append(h.length() == 1 ? "0" + h : h);
+        }
+        else{
+            stringBuilder.append(h.length() == 1 ? "-0" + h : h);
+        }
         stringBuilder.append(":");
         stringBuilder.append(m.length() == 1 ? "0" + m : m);
         stringBuilder.append(":");
         stringBuilder.append(s.length() == 1 ? "0" + s : s);
-
         return stringBuilder.toString();
     }
 }
