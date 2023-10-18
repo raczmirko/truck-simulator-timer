a2d3650995717790fb261309ab3df5843ac52323
raczmirko <m1rk0d3v@gmail.com>
2023-10-18 21:18:17 +0200

    feat: created a simple dialog to show text explaining the functions in settings

--- app/src/main/java/hu/okrim/trucksimulatortimer/SettingsActivity.java
+++ app/src/main/java/hu/okrim/trucksimulatortimer/SettingsActivity.java
@@ -2,15 +2,21 @@ package hu.okrim.trucksimulatortimer;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
     RadioButton radioButtonOptimistic, radioButtonRealistic, radioButtonPessimistic;
+    Button buttonHelpEstimation, buttonHelpSampling;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,11 +73,36 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             }
         });
+        buttonHelpSampling.setOnClickListener(view -> showHelpDialog(R.string.help_sampling));
+        buttonHelpEstimation.setOnClickListener(view -> showHelpDialog(R.string.help_estimation));
     }
 
     private void initComoponents() {
         radioButtonOptimistic = findViewById(R.id.radioButtonOptimistic);
         radioButtonRealistic = findViewById(R.id.radioButtonRealistic);
         radioButtonPessimistic = findViewById(R.id.radioButtonPessimistic);
+        buttonHelpSampling = findViewById(R.id.buttonSettingsHelpSampling);
+        buttonHelpEstimation = findViewById(R.id.buttonSettingsHelpEstimation);
+    }
+
+    public void showHelpDialog(int textReference){
+        // Create the dialog
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SettingsActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.settings_help_dialog_layout, null);
+        dialogBuilder.setView(dialogView);
+
+        // References to the EditText views in the dialog
+        final TextView helpText = dialogView.findViewById(R.id.textViewSettingsHelpDialog);
+        helpText.setTextColor(Color.BLACK);
+        helpText.setTextSize(20);
+        helpText.setText(textReference);
+
+        // Define OK button behavior
+        dialogBuilder.setPositiveButton("OK", (dialog, which) -> {
+            dialog.dismiss();
+        });
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
     }
 }
